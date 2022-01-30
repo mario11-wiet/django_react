009d672a3fcdeded1556fc44933d4ed1bc1b9b94
mario11-wiet <mk.11.03@wp.pl>
2022-01-30 05:39:24 +0100

    Init api

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,7 +1,11 @@
+import imp
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Room
+from .serializers import RoomSerializer
+from rest_framework import generics
 # Create your views here.
 
-def main(request):
-    return HttpResponse("<h1>......</h1>")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
