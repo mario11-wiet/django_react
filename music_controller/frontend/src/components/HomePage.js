05477a1265334b7c3f4321bdf461a868b03a9f0f
mario11-wiet <mk.11.03@wp.pl>
2022-01-31 03:36:58 +0100

    Create join,create sites and api to this sites

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -8,6 +8,7 @@ import {
   } from "react-router-dom";
 import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
+import Room from "./Room";
 
 export default class HomePage extends Component{
     constructor(props){
@@ -22,6 +23,7 @@ export default class HomePage extends Component{
                     <Route exact path="/"><p>HomePage</p></Route>
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/room/:roomCode" component={Room} />
                 </Switch>
             </Router>
         );
