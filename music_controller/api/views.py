3e634843f0aaec3f73758207757986bd9699da1b
mario11-wiet <mk.11.03@wp.pl>
2022-02-01 05:01:15 +0100

    Create Room page, HomePage

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from django.shortcuts import render
 from .models import Room
 from .serializers import RoomSerializer, CreateRoomSerializer
-
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -78,5 +78,26 @@ class CreateRoomView(APIView):
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
         
 
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data ={
+            'code':self.request.session.get('room_code')
+        }
+
+        return JsonResponse(data, status=status.HTTP_200_OK)
 
 
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room_result = Room.objects.filter(host=host_id)
+            if len(room_result) > 0:
+                room = room_result[0]
+                room.delete()
+        
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
